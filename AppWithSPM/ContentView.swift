1c16350cf53663cd214efb68e4973c63e08da7b2
Tomoaki Yagishita <tyagishi@gmail.com>
2020-06-18 11:10:47 +0900

    Initial Commit

---
+++ AppWithSPM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppWithSPM
+//
+//  Created by Tomoaki Yagishita on 2020/06/18.
+//  Copyright © 2020 SmallDeskSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
